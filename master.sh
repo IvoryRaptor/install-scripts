5b6410fb0f60b294789702e39afce5b658e57c11
fjl716 <fjl716@163.com>
2018-07-13 09:37:57 +0800

    安装saltstack

--- master.sh
+++ master.sh
@@ -7,3 +7,8 @@ yum install python-paramiko -y
 sed -i 's/#auto_accept: False/auto_accept: True/g' /etc/salt/master
 
 salt-master start &
+while(true)
+do
+    salt '*' test.ping
+    sleep 5s
+done
